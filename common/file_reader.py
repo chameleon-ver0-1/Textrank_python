9cf54c7a94bafded56cb4cedbf7275c9e7544fe1
onisley <onisley@naver.com>
2019-10-21 17:23:36 +0900

    Modify project Architecture

---
+++ common/file_reader.py
@@ -0,0 +1,74 @@
+import networkx
+import re
+import sys
+
+
+class RawSentence:
+    def __init__(self, textIter):
+        if type(textIter) == str:
+            self.textIter = textIter.split('\n')
+        else:
+            self.textIter = textIter
+        self.rgxSplitter = re.compile('([.!?:](?:["\']|(?![0-9])))')
+
+    def __iter__(self):
+        for line in self.textIter:
+            ch = self.rgxSplitter.split(line)
+            for s in map(lambda a, b: a + b, ch[::2], ch[1::2]):
+                if not s: continue
+                yield s
+
+
+class RawSentenceReader:
+    def __init__(self, filepath):
+
+        self.filepath = filepath
+        self.rgxSplitter = re.compile('([.!?:](?:["\']|(?![0-9])))')
+
+    def __iter__(self):
+        for line in open(self.filepath, encoding='utf-8'):
+            ch = self.rgxSplitter.split(line)
+            for s in map(lambda a, b: a + b, ch[::2], ch[1::2]):
+                if not s: continue
+                yield s
+
+
+class RawTagger:
+    def __init__(self, textIter, tagger=None):
+        if tagger:
+            self.tagger = tagger
+        else:
+            from konlpy.tag import Komoran
+            self.tagger = Komoran()
+        if type(textIter) == str:
+            self.textIter = textIter.split('\n')
+        else:
+            self.textIter = textIter
+        self.rgxSplitter = re.compile('([.!?:](?:["\']|(?![0-9])))')
+
+    def __iter__(self):
+        for line in self.textIter:
+            ch = self.rgxSplitter.split(line)
+            for s in map(lambda a, b: a + b, ch[::2], ch[1::2]):
+                if not s: continue
+                yield self.tagger.pos(s)
+
+
+class RawTaggerReader:
+    def __init__(self, filepath, tagger=None):
+        if tagger:
+            self.tagger = tagger
+        else:
+            from konlpy.tag import Komoran
+            self.tagger = Komoran()
+        self.filepath = filepath
+        self.rgxSplitter = re.compile('([.!?:](?:["\']|(?![0-9])))')
+
+    def __iter__(self):
+        for line in open(self.filepath, encoding='utf-8'):
+            # print(line)
+            ch = self.rgxSplitter.split(line)
+            for s in map(lambda a, b: a + b, ch[::2], ch[1::2]):
+                # print(s)
+                if not s: continue
+                yield self.tagger.pos(s)
